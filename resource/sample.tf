3d894cd97a6f167a1a26e0849fc41d6d04808136
paddu <padmajamaanyam323@gmail.com>
2023-12-21 21:16:32 +0530

    Initial commit

--- resource/sample.tf
+++ resource/sample.tf
@@ -1,5 +1,5 @@
 resource "aws_instance""sample"{
-  ami= ami-03265a0778a880afb
+  ami = "ami-03265a0778a880afb"
   instance_type= "t2.micro"
   vpc_security_group_ids = ["vpc-06ad3ac329ee00967"]
   tags={
