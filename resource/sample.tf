00e26df565ea485fae8abd20a3db1e1e1e0df0f6
paddu <padmajamaanyam323@gmail.com>
2023-12-21 21:20:36 +0530

    Initial commit

--- resource/sample.tf
+++ resource/sample.tf
@@ -1,8 +1,7 @@
-resource "aws_instance""sample"{
+resource "aws_instance" "sample"{
   ami = "ami-03265a0778a880afb"
-  instance_type= "t2.micro"
-  vpc_security_group_ids = ["vpc-06ad3ac329ee00967"]
-  tags={
-    Name="hellosp"
+  instance_type = "t2.micro"
+  tags ={
+    Name ="hellosp"
   }
 }
